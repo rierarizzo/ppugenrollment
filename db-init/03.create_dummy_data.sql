c121ebc8cbf6798a7574b5e9488b67221a490896
riera <kenethrr@outlook.com>
2024-01-21 17:53:33 -0500

    Agrega dummy data

--- db-init/03.create_dummy_data.sql
+++ db-init/03.create_dummy_data.sql
@@ -1,6 +1,26 @@
 USE ppugenrollment;
 
-INSERT INTO company (name, ruc, image_url)
-VALUES ('Cobis', '0987293812001', 'no-image'),
-	   ('Banco Pichincha', '0982239281001', 'no-image'),
-	   ('Pronaca', '0922819283001', 'no-image');
+INSERT INTO
+	company (name, ruc, image_url)
+VALUES
+	('Cobis', '0987293812001', 'no-image'),
+	('Banco Pichincha', '0982239281001', 'no-image'),
+	('Pronaca', '0922819283001', 'no-image');
+
+INSERT INTO
+	project (company, name, description, starts, ends)
+VALUES
+	(
+		1,
+		'Sistema financiero',
+		'Sistema financiero desc',
+		now(),
+		now()
+	),
+	(
+		2,
+		'Sistema financiero 2',
+		'Sistema financiero 2 desc',
+		now(),
+		now()
+	);
